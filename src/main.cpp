617f5d46da9cbafd3f5431b4046ba08cbeb65df5
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2018-10-27 12:13:09 +0200

    First triangle

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,21 @@ using namespace std;
 const int WIDTH = 1280;
 const int HEIGHT = 720;
 
+void InitGL() {
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glShadeModel(GL_SMOOTH);
+}
+
+void render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2d(-0.9, -0.9);
+    glVertex2d(0.9, -0.9);
+    glVertex2d(0, 0.9);
+    glEnd();
+}
+
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window *window = SDL_CreateWindow("Native Procedural Cave Generation", SDL_WINDOWPOS_CENTERED,
@@ -22,8 +37,7 @@ int main() {
     SDL_GL_CreateContext(window);
     SDL_GL_SetSwapInterval(1);
 
-    glViewport(0, 0, WIDTH, HEIGHT);
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    InitGL();
 
     bool running = true;
     while (running) {
@@ -36,7 +50,7 @@ int main() {
             }
         }
 
-        glClear(GL_COLOR_BUFFER_BIT);
+        render();
         SDL_GL_SwapWindow(window);
     }
 
